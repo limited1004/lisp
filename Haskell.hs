b94966f6deef2bd56a512cc60ece0d943d07c8b0
limited1004 <55810900+limited1004@users.noreply.github.com>
2020-05-28 10:51:04 +0300

    Update Haskell.hs

--- Haskell.hs
+++ Haskell.hs
@@ -1,5 +1,14 @@
 import Data.List
   
+-- Задача №16
+-- Определите предикат РАВЕНСТВО-МНОЖЕСТВ, проверяющий совпадение двух множеств (независимо от порядка следования элементов). Подсказка:
+-- напишите функцию УДАЛИТЬ, удаляющую данный элемент из множества.
+
+allDifferent :: (Eq a) => [a] -> Bool
+allDifferent list = case list of
+    []      -> True
+    (x:xs)  -> x `notElem` xs && allDifferent xs
+
 -- Задача №18
 -- Определите предикат РАВЕНСТВО-МНОЖЕСТВ, проверяющий совпадение двух множеств (независимо от порядка следования элементов). Подсказка:
 -- напишите функцию УДАЛИТЬ, удаляющую данный элемент из множества.
@@ -31,19 +40,25 @@ qsort (x:xs) = qsort lesser ++ [x] ++ qsort greater
 
 
 main = do 
-       putStrLn "Task №18"
+
+       putStrLn "Task 16"
+       print (allDifferent [5,4,3,2,1,2]) 
+       print (allDifferent [1,2]) 
+       print (allDifferent [1,2,3,4,5,1,4])
+
+       putStrLn "Task 18"
        print (check_lists [5,4,3,2,1,2] [1,2,3,4,2,5]) 
        print (check_lists [1,2] [2,2,1]) 
        print (check_lists [1,2,3,4,5,1,4] [1,2,3,4,4,1])
 
-       putStrLn "Task №19"
+       putStrLn "Task 19"
        print (subSet [3] [1,2,3])
        print (subSet [1] [4,2,3])
        print (subSet [1] [])
        print (subSet [1,2] [1,2])
        print (subSet [1,2] [2,1])
        
-       putStrLn "Task №25"
+       putStrLn "Task 25"
        print (qsort [1,2,3,2,4,1,4])
        print (qsort [1,2,3,5,6,1,4])
        print (qsort [1,2,1,3,4,5,4])
